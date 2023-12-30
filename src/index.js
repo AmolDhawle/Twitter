348907d777bb13e96c676440236bb12cc47e6eb0
Amol Dhawle <amoldhawle2@gmail.com>
2023-12-30 17:34:11 +0530

    Basic twitter setup completed

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const  connect = require("./config/database");
+const TweetRepository = require("./repository/tweetRepository");
+const Comment = require("./models/comment");
+
+const {PORT} = require("./config/serverConfig");
+
+app.listen(PORT, async () => {
+    console.log(`Server started at PORT ${PORT}`);
+    await connect();
+    console.log("MongoDB connected");
+    // const tweet = await Tweet.create({
+    //     content: "Hi this is my first tweet",
+    //     userEmail: "admin@gmail.com"
+    // });
+    const tweetRepo = new TweetRepository();
+    const tweet = await tweetRepo.getWithComments("6590004e9f5538e208878ec9");
+    console.log(tweet);
+}) 
